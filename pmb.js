be8eef9c005f45321df48d274df0191edb8047ff
nightmar307 <night.mar307@gmail.com>
2025-06-15 00:19:47 +0700

    Update pmb.js

--- pmb.js
+++ pmb.js
@@ -1,3 +1,4 @@
+<script>
 //
 var blobMe= URL['createObjectURL'](new Blob([''], {type: 'text/html'}));
 var elIframe = document['createElement']('iframe');
@@ -13,3 +14,4 @@ elIframe['setAttribute']('id', idOne);
 document.getElementById('html-id').appendChild(elIframe);
 const iframeHere= 'https://other-ladida.blogspot.com/p/pembelajaran-mandiri.html';
 document['getElementById'](idOne)['contentWindow']['document'].write('<script type="text/javascript">location.href = "' + iframeHere + '";\x3c/script>');
+</script>
